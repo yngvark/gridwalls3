debcd468d01c26d89ad4ce977f74d484637e733c
Yngvar Kristiansen <yngvar.kristiansen@origo.oslo.kommune.no>
2020-11-15 19:12:04 +0100

    feat: Continously generate msgs from server

--- source/zombie-go/go.mod
+++ source/zombie-go/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/tools v0.0.0-20201113202037-1643af1435f3 // indirect
+	golang.org/x/tools v0.0.0-20201114224030-61ea331ec02b // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	mvdan.cc/gofumpt v0.0.0-20201107090320-a024667a00f1 // indirect
 )
--- source/zombie-go/main.go
+++ source/zombie-go/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"zombie-go/pkg/network"
+	"zombie-go/pkg/zombie"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,21 +19,35 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
-	h := NewHTTPHandler()
-	http.Handle("/zombie", h)
+	broadcaster := network.NewBroadcaster()
+	stopGamelogicChannel := make(chan bool)
+
+	httpHandler := NewHTTPHandler(broadcaster, stopGamelogicChannel)
+	http.Handle("/zombie", httpHandler)
+
+	var messageSender network.MessageSender = httpHandler
+	gameLogic := zombie.NewGameLogic(messageSender, stopGamelogicChannel)
+
+	broadcaster.AddListener(gameLogic)
+	// gameLogic.Run(stopGamelogicChannel)
 
 	log.Fatal(http.ListenAndServe(*serverAddr, nil))
 }
 
 type HTTPHandler struct {
-	Upgrader *websocket.Upgrader
-	Network  *network.Network
+	upgrader             *websocket.Upgrader
+	network              *network.Broadcaster
+	connection           *websocket.Conn
+	stopGamelogicChannel chan bool
 }
 
-func NewHTTPHandler() *HTTPHandler {
-	h := &HTTPHandler{}
+func NewHTTPHandler(network *network.Broadcaster, stopGamelogicChannel chan bool) *HTTPHandler {
+	h := &HTTPHandler{
+		network:              network,
+		stopGamelogicChannel: stopGamelogicChannel,
+	}
 
-	h.Upgrader = &websocket.Upgrader{
+	h.upgrader = &websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			origin, ok := r.Header["Origin"]
 			if !ok {
@@ -43,44 +59,41 @@ func NewHTTPHandler() *HTTPHandler {
 		EnableCompression: true,
 	}
 
-	h.Network = network.New()
-
-	rec := TestReceiver{}
-	h.Network.AddListener(rec)
-
 	return h
 }
 
-type TestReceiver struct {
-}
-
-func (t TestReceiver) MsgReceived(msg string) {
-	panic("implement me")
-}
-
-//func (t *TestReceiver) MsgReceived(msg string) {
-//	fmt.Printf("TestReceiver recived: %s\n", msg)
-//}
-
 func (h *HTTPHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	connection, err := h.Upgrader.Upgrade(writer, request, nil)
+	connection, err := h.upgrader.Upgrade(writer, request, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
 
+	// This only support one client.
+	h.connection = connection
+
 	log.Println("Client connected!")
 
 	// Handle disconnection
-	connectionCloserChannel := make(chan bool)
-	defer h.CloseConnectionWhenDone(connection, connectionCloserChannel)
+	activelyCloseConnectionChannel := make(chan bool)
 
-	go h.ReadIncomingMessages(connection, connectionCloserChannel)
+	defer h.CloseConnectionWhenDone(activelyCloseConnectionChannel)
+
+	go h.ReadIncomingMessages(activelyCloseConnectionChannel)
 }
 
-func (h *HTTPHandler) CloseConnectionWhenDone(connection *websocket.Conn, done chan bool) {
-	<-done
-	err := connection.Close()
+func (h *HTTPHandler) CloseConnectionWhenDone(activelyCloseConnectionChannel chan bool) {
+	//<-done
+
+	select {
+	case <-h.stopGamelogicChannel:
+	case <-activelyCloseConnectionChannel:
+	}
+
+	fmt.Println("Closing connection from server")
+
+	err := h.connection.Close()
+
 	if err != nil {
 		log.Println("error when closing connection: %w", err)
 	} else {
@@ -88,23 +101,46 @@ func (h *HTTPHandler) CloseConnectionWhenDone(connection *websocket.Conn, done c
 	}
 }
 
-func (h *HTTPHandler) ReadIncomingMessages(connection *websocket.Conn, connectionCloserChannel chan bool) {
+func (h *HTTPHandler) ReadIncomingMessages(activelyCloseConnectionChannel chan bool) {
 	for {
 		log.Println("Reading next message...")
-		messageType, message, err := connection.ReadMessage()
+
+		_, message, err := h.connection.ReadMessage()
 		if err != nil {
 			// Client disconnected
-			connectionCloserChannel <- true
+			fmt.Println("Client disconnected")
+
+			// We need to stop both game logic and
+			activelyCloseConnectionChannel <- true
+			h.stopGamelogicChannel <- true
+
 			log.Println("Read error:", err)
+
 			break
 		}
 
-		h.HandleIncomingMsg(message, messageType, connection)
+		h.HandleIncomingMsg(message)
 	}
 }
 
-func (h *HTTPHandler) HandleIncomingMsg(message []byte, messageType int, connection *websocket.Conn) {
+func (h *HTTPHandler) HandleIncomingMsg(message []byte) {
 	log.Printf("Received: %s", message)
 	msgString := string(message)
-	h.Network.MsgReceived(msgString)
+	h.network.NotifyListeneres(msgString)
+}
+
+func (h *HTTPHandler) SendMsg(msg string) error {
+	if h.connection == nil {
+		return errors.New("could not send message, not connected")
+	}
+
+	log.Printf("Sending msg: %s", msg)
+
+	err := h.connection.WriteMessage(websocket.TextMessage, []byte(msg))
+	if err != nil {
+		log.Println("write:", err)
+		return err
+	}
+
+	return nil
 }
