dea264b1a3b673c59c1ead4203f869731cb2b7c4
Yngvar Kristiansen <yngvar.kristiansen@origo.oslo.kommune.no>
2020-11-28 10:55:22 +0100

    feat(frontend): WIP: Attempt to get retarded webpack to work

--- source/front-end2/src/game/index.ts
+++ source/front-end2/src/game/index.ts
@@ -28,3 +28,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 }, false);
 
+console.log('index!!')
--- source/front-end2/src/webpack.common.js
+++ source/front-end2/src/webpack.common.js
@@ -1,14 +1,22 @@
 // https://stackoverflow.com/questions/36039146/webpack-dev-server-compiles-files-but-does-not-refresh-or-make-compiled-javascri
+const path = require('path')
+
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'dist/phaser.js')
 
 module.exports = {
     // context: __dirname + "/game",
-    entry: "./game/index.ts",
+    // entry: ['./game/index.ts'],
+    entry: {
+        main: "./game/index.ts",
+        vendor: ['phaser']
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
+                include: path.join(__dirname, '../src'),
                 use: 'ts-loader',
-                exclude: /node_modules/
             }
         ]
     },
@@ -16,9 +24,25 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         publicPath: "/dist",
-        filename: "bundle.js"
+        filename: "[name].bundle.js",
+        chunkFilename: '[name].chunk.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    }
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            'phaser': phaser,
+        }
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    filename: '[name].bundle.js'
+                }
+            }
+        }
+    },
 }
