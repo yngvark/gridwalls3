2052d3d64136c338b97f8e05e622141e8d7eeeb2
Yngvar Kristiansen <yngvar.kristiansen@origo.oslo.kommune.no>
2020-11-29 14:11:08 +0100

    feat(frontend): Support local https

--- source/front-end3/server/node_server.ts
+++ source/front-end3/server/node_server.ts
@@ -1,30 +1,51 @@
 import { config } from 'dotenv'
 config()
 
-import {getServer} from "./app"
+import {getApp} from "./app"
 import express from "express"
+import http from "http"
 import https from "https"
 import fs from "fs"
-import path from "path"
 
-let app:express.Application = getServer()
+const PORT = getEnv("PORT")
+const BACKEND_URL = getEnv("BACKEND_URL")
 
-if (!process.env.CERTIFICATE_FILE || !process.env.KEY_FILE) {
-    throw new Error('SSL requires both a certificate and a key')
+function getEnv(name:string) {
+    let e = process.env[name]
+    if (!e) throw new Error(`Missing environment variable: ${name}`)
+
+    return e
 }
 
-const certificate = fs.readFileSync(process.env.CERTIFICATE_FILE, 'utf8')
-const privateKey = fs.readFileSync(process.env.KEY_FILE!, 'utf8')
+let useHttps = false
+if (process.env.CERTIFICATE_FILE || process.env.KEY_FILE) {
+    if (!process.env.CERTIFICATE_FILE || !process.env.KEY_FILE) {
+        throw new Error('SSL requires both a certificate and a key')
+    }
 
-let credentials =  {
-    key: privateKey,
-    cert: certificate
+    useHttps = true
 }
 
-let server = https.createServer(credentials, app)
+let app:express.Application = getApp(BACKEND_URL)
+let protocol:string
+let server : http.Server | https.Server
 
-const port = 3001
+if (!useHttps) {
+    protocol = 'http'
+    server = http.createServer(app)
+} else {
+    const certificate = fs.readFileSync(process.env.CERTIFICATE_FILE!, 'utf8')
+    const privateKey = fs.readFileSync(process.env.KEY_FILE!, 'utf8')
 
-server.listen(port, () => {
-    console.log(`Example app listening at https://localhost:${port}`)
+    let credentials =  {
+        key: privateKey,
+        cert: certificate
+    }
+
+    protocol = 'https'
+    server = https.createServer(credentials, app)
+}
+
+server.listen(PORT, () => {
+    console.log(`Example app listening at ${protocol}://localhost:${PORT}`)
 })
