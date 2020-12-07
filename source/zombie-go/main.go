6d45477f6c45df67adbd33c1e73976f4394ea4a8
Yngvar Kristiansen <yngvar.kristiansen@origo.oslo.kommune.no>
2020-12-07 20:58:51 +0100

    chore(zombie): Extract httphandler

--- source/zombie-go/main.go
+++ source/zombie-go/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/yngvark/gridwalls3/source/zombie-go/pkg/mainhelp"
@@ -11,8 +10,6 @@ import (
 
 	"github.com/yngvark/gridwalls3/source/zombie-go/pkg/network"
 	"github.com/yngvark/gridwalls3/source/zombie-go/pkg/zombie"
-
-	"github.com/gorilla/websocket"
 )
 
 func main() {
@@ -53,13 +50,6 @@ func setupGame(allowedCorsOrigins map[string]bool) {
 	httpHandler := NewHTTPHandler(allowedCorsOrigins, broadcaster, stopGamelogicChannel)
 	http.Handle("/zombie", httpHandler)
 
-	http.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
-		out := []byte("OK")
-		_, err := writer.Write(out)
-
-		fmt.Println("error when responding on /health: %s", err)
-	})
-
 	var messageSender network.MessageSender = httpHandler
 	gameLogic := zombie.NewGameLogic(messageSender, stopGamelogicChannel)
 
@@ -67,123 +57,14 @@ func setupGame(allowedCorsOrigins map[string]bool) {
 }
 
 func listenAndServe(port string) {
+	http.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
+		out := []byte("OK")
+		_, err := writer.Write(out)
+
+		fmt.Println("error when responding on /health: %s", err)
+	})
+
 	serverAddr := flag.String("addr", fmt.Sprintf(":%s", port), "http service address")
 	log2().Infof("Running on %s\n", *serverAddr)
 	log.Fatal(http.ListenAndServe(*serverAddr, nil))
-}
-
-type HTTPHandler struct {
-	upgrader             *websocket.Upgrader
-	network              *network.Broadcaster
-	connection           *websocket.Conn
-	stopGamelogicChannel chan bool
-}
-
-func NewHTTPHandler(allowedOrigins map[string]bool, network *network.Broadcaster, stopGamelogicChannel chan bool) *HTTPHandler {
-	h := &HTTPHandler{
-		network:              network,
-		stopGamelogicChannel: stopGamelogicChannel,
-	}
-
-	h.upgrader = &websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			origin, ok := r.Header["Origin"]
-			if !ok {
-				return false
-			}
-
-			if len(origin) > 0 {
-				_, ok := allowedOrigins[origin[0]]
-				log.Printf("Checking origin %s. Result: %t\n", origin[0], ok)
-
-				return ok
-			}
-
-			return true
-		},
-		EnableCompression: true,
-	}
-
-	return h
-}
-
-func (h *HTTPHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	connection, err := h.upgrader.Upgrade(writer, request, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-
-	// This only support one client.
-	h.connection = connection
-
-	log.Println("Client connected!")
-
-	// Handle disconnection
-	activelyCloseConnectionChannel := make(chan bool)
-
-	defer h.CloseConnectionWhenDone(activelyCloseConnectionChannel)
-
-	go h.ReadIncomingMessages(activelyCloseConnectionChannel)
-}
-
-func (h *HTTPHandler) CloseConnectionWhenDone(activelyCloseConnectionChannel chan bool) {
-	select {
-	case <-h.stopGamelogicChannel:
-	case <-activelyCloseConnectionChannel:
-	}
-
-	fmt.Println("Closing connection from server")
-
-	err := h.connection.Close()
-
-	if err != nil {
-		log.Println("error when closing connection: %w", err)
-	} else {
-		log.Println("Connection closed successfully.")
-	}
-}
-
-func (h *HTTPHandler) ReadIncomingMessages(activelyCloseConnectionChannel chan bool) {
-	for {
-		log.Println("Reading next message...")
-
-		_, message, err := h.connection.ReadMessage()
-		if err != nil {
-			// Client disconnected
-			fmt.Println("Client disconnected")
-
-			// We need to stop both game logic and
-			activelyCloseConnectionChannel <- true
-			h.stopGamelogicChannel <- true
-
-			log.Println("Read error:", err)
-
-			break
-		}
-
-		h.HandleIncomingMsg(message)
-	}
-}
-
-func (h *HTTPHandler) HandleIncomingMsg(message []byte) {
-	log.Printf("Received: %s", message)
-	msgString := string(message)
-	h.network.NotifyListeneres(msgString)
-}
-
-func (h *HTTPHandler) SendMsg(msg string) error {
-	if h.connection == nil {
-		return errors.New("could not send message, not connected")
-	}
-
-	log.Printf("Sending msg: %s", msg)
-
-	err := h.connection.WriteMessage(websocket.TextMessage, []byte(msg))
-	if err != nil {
-		log.Println("write:", err)
-		return err
-	}
-
-	return nil
 }
